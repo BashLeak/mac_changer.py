3c040e181b722829137812bff5dd8583ec773e8e
BashLeak <100306796+BashLeak@users.noreply.github.com>
2022-03-07 18:43:44 +0300

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,14 +1,36 @@
 #for linux
 import os
 import subprocess
+import random
 
 ata = ""
 dene = os.listdir('/sys/class/net/')
+dizi = []
+tata = ""
+dizia = [":"]
+
+for i in range(10):
+    i += 1
+    dizi.append(random.randint(0,9))
+
+    if (i%2 == 0):
+      dizi.extend(dizia)
+
+dizi.insert(0,0)
+dizi.insert(1,0)
+dizi.insert(2,":")
+dizi.pop()
+
+for i in dizi:
+
+    tata = tata+str(i)
 
 for i in dene:
     ata = ata+str(i)+" "
 
-arayuz = input("mac adresini değiştirmek istediğiniz "+ata + "arayüzlerinden birini giriniz: ")
+arayuz = input("mac adresini değiştirmek istediğiniz "+ata + " arayüzlerinden birini giriniz: ")
+
+print("mac adresi "+tata+" dır")
 
 while True:
 
@@ -16,7 +38,7 @@ while True:
 
         subprocess.call(["ifconfig",arayuz,"down"])
 
-        subprocess.call(["ifconfig",arayuz,"hw","ether","00:34:87:44:12:92"])
+        subprocess.call(["ifconfig",arayuz,"hw","ether",tata])
 
         subprocess.call(["ifconfig",arayuz,"up"])
 
